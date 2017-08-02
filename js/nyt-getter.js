075513d458e3ff571a18944c2136de4e1dcaaaaa
Rob-Rychs <Robrychel@gmail.com>
2017-08-02 14:54:24 -0700

    refactored nyt-getter to es6

--- js/nyt-getter.js
+++ js/nyt-getter.js
@@ -1,4 +1,4 @@
-$(function(){
+$( () =>{
 
   $('#category').on('change', function(event){
     event.preventDefault();
@@ -7,41 +7,34 @@ $(function(){
     $('.logo_image').addClass('resized');
     $('.articles').css('display', 'flex');
     $('.loader').css('display', 'block');
+    
+    // Get user input from select field
+    let input = $('#category').val();
 
-    var input = $('#category').val();
-
-    var geturl = 'https://api.nytimes.com/svc/topstories/v2/' + input + '.json';
-    geturl += '?' + $.param({
-      'api-key': '54d43868054141fc8972cfb3facb34c3'
-    });
+    // build our geturl
+    let geturl = `https://api.nytimes.com/svc/topstories/v2/${input}.json?api-key=54d43868054141fc8972cfb3facb34c3`
     $.ajax({
       url: geturl,
       method: 'GET',
-    }).done(function(data) {
+    }).done(data => {
        function hasImage(entry){
          return entry.multimedia.length
        }
-       var newArray = data.results.filter(hasImage)
+       let newArray = data.results.filter(hasImage)
        newArray.splice(12);
 
-       $.each(newArray, function(key, value){
-        var imgurl = value.multimedia[4].url;
-        var txt = value['abstract'];
-        var link = value.url;
-        var listitem = '<li class="article-item"><a class="article-link" href="'+link+'" target="blank"><div class="article-wrapper fade-in-on-load" style="background-image:url('+imgurl+')"><div class="article-text"><p>'+txt+'</p></div></div></a></li>';
-        console.log(listitem);
-        console.log(imgurl);
-
-
+       $.each(newArray, (key, value) => {
+        let imgurl = value.multimedia[4].url;
+        let txt = value['abstract'];
+        let link = value.url;
+        let listitem = `<li class="article-item"><a class="article-link" href="${link}" target="blank"><div class="article-wrapper fade-in-on-load" style="background-image:url(${imgurl})"><div class="article-text"><p>${txt}</p></div></div></a></li>`;
+        
         $('.articles-list').append(listitem);
         $('.loader').hide();
         
-        
-
       });
-    }).fail(function(){
+    }).fail(()=>{
        $('.articles-list').append('<li> Sorry there was an error </li>')  
     });
-
   });
 });
